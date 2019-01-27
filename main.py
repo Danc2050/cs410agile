763c8dd5526c07c94c29d4d1fe30ad79413e6bb5
Danc2050 <danc2@pdx.edu>
2019-01-26 22:53:01 -0800

    Commented code for main.py

--- main.py
+++ main.py
@@ -11,22 +11,33 @@ if __name__ == "__main__":
 
         # Login attempt
         if len(sys.argv) != 2:
-            print("Incorrect sftp command.")
+            print("Sftp program must take one valid argument.")
             exit(1)
+
+        # Edge case for splitting the destination address if an '@'
+        # sign exists
         tup = sys.argv[1].split("@")
-        if(len(tup) == 2): # if .split fails, then
+
+        # If .split succeeds, a '@' is in the sftp destination
+        # and then the tuple returned will be {[user], [hostname]}
+        # Set each respective field to a local variable
+        if len(tup) == 2:
             hostname = tup[1]
             username = tup[0]
+
+        # Otherwise, we need to get the host machines USER variable for username.
+        # The hostname will be inside sys.argv[1] (e.g., dylanlaufenberg.com)
         else:
             username = getpass.getuser()
             hostname = sys.argv[1]
 
-        # Printing credentials and asking for password
+        # Printing credentials to user and asking for password from user
+        # getpass.getpass() does not echo the input... a security measure
         print(username+"@"+hostname+"'s", end=' ', flush=True)
         password = getpass.getpass()
         sftp = login.login(hostname, username, password)
 
-        # Type check
+        # Type check for correct allocation of pysftp.Connection object
         if type(sftp) == pysftp.Connection:
             print("Authentication success.")
             # Run the main controller loop, and when it returns, pass along its
