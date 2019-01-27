80637a0f97707087b8ba29add160aa3c723ff1af
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-01-27 10:12:37 -0800

    Clean up and augment existing tests

--- tests/unit/test_login.py
+++ tests/unit/test_login.py
@@ -1,14 +1,39 @@
 import pysftp
 from actions import login
-from tests import test_server
+from tests.test_server import HOSTNAME, USERNAME, PASSWORD
 import pytest
 import paramiko
-#
 
-# Login with test_server credentials (should work if file is loaded)
+
 def test_real_login():
-    assert type(login.login(test_server.HOSTNAME, test_server.USERNAME, test_server.PASSWORD)) is pysftp.Connection
-# Login without proper credentials
-def test_fake_login():
-    with pytest.raises(paramiko.ssh_exception.SSHException):
-        login.login("Fake", "Fake", "Fake")
+    """Tests login() with test_server credentials, which should result
+    in a successful connection.
+    """
+
+    sftp = login.login(HOSTNAME,
+                       USERNAME,
+                       PASSWORD)
+    assert type(sftp) is pysftp.Connection
+
+
+def test_login_with_invalid_credentials(capsys):
+    """Verify that calling login() with invalid credentials fails."""
+
+    # List of test cases. Each case is of the form:
+    #
+    #   (host, username, password)
+    test_cases = [
+        (None, None, None),
+        ("", "", ""),
+        ("Fake", "Fake", "Fake"),
+        (HOSTNAME, "Fake", "Fake"),
+        (HOSTNAME, "cs510andrew", "zFa8FkS7CC7eG8fx"),  # Using Anthony's PW
+        ("linuxlab.cs.pdx.edu", USERNAME, PASSWORD),
+        ("sandwich dot com", USERNAME, PASSWORD),
+        ("test@example.com", USERNAME, PASSWORD)
+    ]
+
+    # For each test case, verify that the same exception is raised.
+    for host, username, password in test_cases:
+        with pytest.raises(paramiko.ssh_exception.SSHException):
+            login.login(host=host, username=username, password=password)
