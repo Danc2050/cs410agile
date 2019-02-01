4523cc7e0d89345879688b870f8d23151763c0be
Daniel Connelly <Danc2@pdx.edu>
2019-01-31 19:12:31 -0800

    Update close.py

--- actions/close.py
+++ actions/close.py
@@ -6,7 +6,6 @@ from app import login
 def close(sftp):
     """Processes the close connection request. Note that pysftp does not
        return any kind of error if the close is succesful.
-       If a connection did not close, however, this code raises a "home made"
-       exception
+       If a connection did not close, however, this code raises an exception
     """
     return sftp.close()
