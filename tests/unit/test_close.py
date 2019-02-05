3450cfdc64564185d1986dc5c6b0957e62e394e7
Daniel Connelly <Danc2@pdx.edu>
2019-02-04 16:30:48 -0800

    Update test_close.py

--- tests/unit/test_close.py
+++ tests/unit/test_close.py
@@ -1,11 +1,14 @@
 from actions import close
 from app import login
+from tests.test_server import HOSTNAME, USERNAME, PASSWORD
 
 def test_real_close():
     """Tests close() when logged onto an active, authentic connection. Ideal behavior
        is returning None type.
     """
-    sftp = login.login("dylanlaufenberg.com", "cs510daniel", "EPyhcYXJtrSgm7zi")
+    sftp = login.login(HOSTNAME,
+                       USERNAME,
+                       PASSWORD)
     assert close.close(sftp) is None
 
 
@@ -17,5 +20,4 @@ def test_close_with_invalid_credentials():
         sftp = login.login("FAKE", "FAKE", "FAKE")
         assert close.close(sftp) == Exception
     except Exception as error:
-        print("test_close_with_invalid_credentials() has failed; credentials are valid or " +
-              "pysftp.close has an error.")
+        return
