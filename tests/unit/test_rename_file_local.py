badbc827f3d751c97d2d0811de8eb0447d876eb2
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-02-18 13:24:00 -0800

    Fix minor typos and do a little source clean-up in unit test

--- tests/unit/test_rename_file_local.py
+++ tests/unit/test_rename_file_local.py
@@ -1,38 +1,32 @@
 import os
-import pytest
-import imp
-import paramiko
-import contextlib
-from actions import rename_files_local 
+from actions import rename_files_local
 
 
 def test_local_file_rename():
-	'''
+	"""
 		Standard output of files is redirected and stored in stdout_string
 		and checked if README.md file is listed while listing local files and
 		directories
-	'''
+	"""
+
 	# Original file name
 	file_test='rename.txt'
 
 	# New renamed file name
 	renamed_file='renamed.txt'
 
-	# List file and directories in cs410agile project folder
-	path='.'
-
 	# Check to handle FileNotFound Exception
 	# Create a file to rename, if it doesn't exist
 	if not os.path.isfile(file_test):
 		open(file_test, 'a').close()
-	# Call rename action to reanme filr from file_test to renamed_file
-	rename_files_local.rename_local_file(file_test,renamed_file)
+	# Call rename action to rename file from file_test to renamed_file
+	rename_files_local.rename_local_file(file_test, renamed_file)
 
 	# Assert true if renamed_file is present on file path
 	assert os.path.isfile(renamed_file) 
 
 	# Assert true if file_test is no longer present on file path
-	# i.e., file_test is reanmed to renamed_file
+	# i.e., file_test is renamed to renamed_file
 	assert os.path.isfile(file_test) == False
 
 	# Clean-up
