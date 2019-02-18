b5d7660267fc7dde8d16a69f1dff1b88147cd301
shwetapaliwal28 <spaliwal@pdx.edu>
2019-02-18 13:03:46 -0800

    added lrename to Controller

--- app/controller.py
+++ app/controller.py
@@ -40,6 +40,8 @@ def main_loop(sftp: pysftp.Connection) -> int:
                 sftp.listdir(".")
             elif len(tokens) == 2 and tokens[0] == "put":
                 put_file_onto_remote_server.put(sftp, tokens[1])
+            elif len(tokens) == 3 and tokens[0] == "lrename":
+                rename_files_local.rename_local_file(tokens[1], tokens[2])
             elif len(tokens) == 1 \
                     and (tokens[0].lower() == "exit"
                          or tokens[0].lower() == "bye"
