77b9e5fdedef520d8b310541dc03de17d978c71f
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-02-27 17:09:04 -0800

    Update ? command. (Will need to update further after merging changes from put_folder_onto_remote_server later.)

--- actions/list_commands.py
+++ actions/list_commands.py
@@ -3,6 +3,7 @@
 def list_commands():
     print(
         "ls - show files\n"
+        "rename - renames files locally\n"
         "rm - remove file\n"
         "put - put a file or folder onto server\n"
 
