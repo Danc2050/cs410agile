147f1c90f459ba4589e0ae7d332ec9f759cb5e7e
shwetapaliwal28 <spaliwal@pdx.edu>
2019-02-27 22:42:40 -0800

    added lrename and rename to the list

--- actions/list_commands.py
+++ actions/list_commands.py
@@ -3,7 +3,9 @@
 def list_commands():
     print(
         "ls - show files\n"
+        "lrename - rename local files"
         "rm - remove file\n"
+        "rename - rename remote files\n"
         "put - put a file or folder onto server\n"
 
     )
