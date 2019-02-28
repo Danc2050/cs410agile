797c3f54b252bc868c0c783a3ea8084c8e4ffefb
shwetapaliwal28 <spaliwal@pdx.edu>
2019-02-27 22:03:10 -0800

    updated error handling

--- actions/rename_file_remote.py
+++ actions/rename_file_remote.py
@@ -1,7 +1,6 @@
 #import required modules
 import pysftp
 
-IO_ERROR_MESSAGE = "Unable to rename file, IO Error is raised."
 ERROR_MESSAGE = "Couldn't rename file! Please check the file name and try again later."
 
 def rename_remote_file(sftp: pysftp.Connection, before:str, after:str):
@@ -17,12 +16,6 @@ def rename_remote_file(sftp: pysftp.Connection, before:str, after:str):
 		# If rename is unsuccessful, then None is returned
 		sftp.rename(before,after)
 		return True
-	except IOError:
-		print(IO_ERROR_MESSAGE)
-	except FileNotFoundError:
-		print(ERROR_MESSAGE)
-		return False
-	except OSError:
-		print("You might not have permissions to rename this file.")
-		print("Please check permissions and try again.")
+	except OSError as e:
+    	print(ERROR_PREFACE, e.strerror)
 		return False
