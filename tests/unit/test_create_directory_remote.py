18ed50acc98b274b8ae54dd0b5e74694d6ae2246
shwetapaliwal28 <spaliwal@pdx.edu>
2019-02-28 13:34:26 -0800

    added test cases

---
+++ tests/unit/test_create_directory_remote.py
@@ -0,0 +1,40 @@
+# Import required libraries
+import pysftp
+import pytest
+import paramiko
+from actions import create_directory_on_remote as mkdirectory
+
+
+def test_remote_create_directory(sftp):
+	"""
+		Test to check if the file is renamed by rename_remote_file action.
+		This test first create a file on remote server, rename it and then remove 
+		it once the test assertion is complete.
+	"""
+
+	# Directory name to create
+	#dir_test='dir_test'
+	dir_test='directory_test'
+
+	# Call mkdir function to create directory on remote server
+	mkdirectory.create_dir_remote(sftp, dir_test)
+
+	# Check if dir_test is created on remote server
+	assert sftp.isdir(dir_test) is True
+
+	# Clean-up (remove newly crteated directory from remote server)
+	sftp.rmdir(dir_test)
+
+def test_remote_create_directory_invalid(sftp,capsys):
+	"""
+		Test to check if pysftp.mkdir can handle invalid directory names and 
+		empty string directory names.
+	"""
+	cases = ["http://www.google.com", ""]
+	for dir_test in cases:
+		# return type of pysftp rename function is None
+		assert mkdirectory.create_dir_remote(sftp, dir_test) is False
+		# cases should raise OSError
+		assert mkdirectory.ERROR_PREFACE in capsys.readouterr().out
+
+	
