0d606d0e09c275ac3574e55e64e2c5770d1a800d
shwetapaliwal28 <spaliwal@pdx.edu>
2019-02-27 23:20:48 -0800

    added entry for mkdir

--- actions/list_commands.py
+++ actions/list_commands.py
@@ -3,6 +3,7 @@
 def list_commands():
     print(
         "ls - show files\n"
+        "mkdir - create directory on remote server"
         "rm - remove file\n"
         "put - put a file or folder onto server\n"
 
