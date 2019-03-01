e8350671504f3f525123ac215a3c9cb89e262c50
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-03-01 15:48:08 -0800

    Add quit/exit/bye to ? command

--- actions/list_commands.py
+++ actions/list_commands.py
@@ -4,9 +4,12 @@ def list_commands():
     print(
         # Note to developers: please sort alphabetically for ease of use
         # and please adjust spacing as needed.
+        "bye\t\tclose the connection and exit\n"
+        "exit\t\tclose the connection and exit\n"
         "lls\t\tlist files on local machine\n"
         "put file\tput a file onto server\n"
         "put -r folder\tput a folder onto server recursively\n"
+        "quit\t\tclose the connection and exit\n"
         "rm file\t\tremove file\n"
     )
     return True
