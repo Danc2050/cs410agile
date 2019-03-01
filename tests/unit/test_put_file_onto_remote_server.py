eb291a6657e5a56b183ad23440569a8133fae2ca
Molly <pixieofhugs@gmail.com>
2019-03-01 11:59:52 -0800

    Merge branch 'master' of github.com: pixieofhugs/cs410agile into delete_from_remote_server
    Requested unit test added

--- tests/unit/test_put_file_onto_remote_server.py
+++ tests/unit/test_put_file_onto_remote_server.py
@@ -8,9 +8,9 @@ def test_no_file(sftp, capsys):
     """Test what happens when you ask to transfer a
     file which doesn't exist.
     """
-
     assert put.put(sftp, "totes.not.fake.no.really") is False
     assert put.ERROR_MESSAGE in capsys.readouterr().out
+
 
 def test_best_case(sftp):
     """This tests what happens if you put a file that does exist on
