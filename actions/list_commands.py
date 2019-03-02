46329163c7c860cbf5e771fd7853846cfa04db50
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-03-01 16:07:28 -0800

    Update ? command

--- actions/list_commands.py
+++ actions/list_commands.py
@@ -2,9 +2,9 @@
 
 def list_commands():
     print(
-        "ls - show files\n"
+        "lls - show files on the local computer\n"
+        "ls - show files on the remote computer\n"
         "rm - remove file\n"
         "put - put a file or folder onto server\n"
-
     )
     return True
