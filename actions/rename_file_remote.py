aa470b3b89e1663f35036dc2ac8f5f30fd09de3a
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-03-02 15:52:37 -0800

    Add integration tests. Tweak default error message for rename action. All tests pass.

--- actions/rename_file_remote.py
+++ actions/rename_file_remote.py
@@ -18,7 +18,7 @@ def rename_remote_file(sftp: pysftp.Connection, before:str, after:str):
 		return True
 	except OSError as e:
 		if e.strerror is None:
-			print(ERROR_PREFACE, "could not rename file.")
+			print(ERROR_PREFACE, "could not rename file or directory.")
 		else:
 			print(ERROR_PREFACE, e.strerror)
 		return False
