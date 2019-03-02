22d346b9982e80145f4d6e793e0102d2204b2a3b
Anthony <anthonynamba@gmail.com>
2019-03-02 12:32:02 -0800

    Merge branch 'master' of https://github.com/pixieofhugs/cs410agile Fixing tests, adding exceptions

--- actions/put_multiple_files_onto_remote_server.py
+++ actions/put_multiple_files_onto_remote_server.py
@@ -1,7 +1,9 @@
 import pysftp
 import actions.put_file_onto_remote_server as put
 
-# This puts a
+# This puts multiple files onto the server. It attempts to put each valid file found
+# onto the server and gives an error message for each file not found. If more than
+# 0 files are added it is considered a success.
 
 ERROR_MESSAGE = "Sorry, we couldn't find your file. Please check your spelling and try again"
 
