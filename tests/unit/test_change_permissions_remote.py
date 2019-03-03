054f39074547bff57c935bb00a72e9d12e6f9138
Molly <pixieofhugs@gmail.com>
2019-03-03 13:47:24 -0800

    Unit tests I have touched are all good and passing.

--- tests/unit/test_change_permissions_remote.py
+++ tests/unit/test_change_permissions_remote.py
@@ -2,7 +2,7 @@ from actions import change_permissions_remote as change
 from actions import put_file_onto_remote_server as put
 
 # Permissions that need changes
-APPROVE = 0x0111
+PERMISSION = 777
 REAL_FILE = "main.py"
 
 
@@ -10,7 +10,7 @@ def test_no_file(sftp, capsys):
     """Test what happens when you ask to change the permissions on a
     file which doesn't exist.
     """
-    assert change.change_permissions(sftp, "totes.not.fake.no.really", APPROVE) is False
+    assert change.change_permissions(sftp, "totes.not.fake.no.really", PERMISSION) is False
     assert change.ERROR_MESSAGE in capsys.readouterr().out
 
 
@@ -18,12 +18,13 @@ def test_restrictive_permissions_case(sftp):
     """This tests if you try to change permissions on something you shouldn't
     have access to
     """
-    assert change.change_permissions(sftp, "/home", APPROVE) is False
+    assert change.change_permissions(sftp, "/home", PERMISSION) is False
     # try to modify and verify it won't do anything
 
 
 def test_allowing_permissions_case(sftp):
-    sftp.put(sftp, REAL_FILE)
-    assert change.change_permissions(sftp, REAL_FILE, APPROVE) is True
+    filename = "testAllowingPermissionCaseSnuggleKittens"
+    sftp.open(filename, "a").close()
+    assert change.change_permissions(sftp, filename, PERMISSION) is True
     # try to modify and verify it will do something!
-    sftp.remove(sftp, REAL_FILE)
+    sftp.remove(filename)
