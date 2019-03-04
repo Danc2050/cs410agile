37057264d6e8ebd8b73c26f699d71c57a1530bb7
Danc2050 <danc2@pdx.edu>
2019-03-04 12:27:28 -0800

    Second permission test done. All tests pass.

--- tests/unit/test_searchLocal.py
+++ tests/unit/test_searchLocal.py
@@ -1,16 +1,19 @@
 from actions.searchLocal import searchLocal
 import os
-import shutil
+
 
 def test_search_existing_file():
     flag = searchLocal("test")
     assert flag > 0
 
+
 def test_search_non_existant_file():
     flag = searchLocal("alkdsjhfasdklfhadshfaldksf")
     assert flag < 1
 
+
 def test_no_permission_in_current_folder():
+    # create directories
     os.mkdir("no_perm")
     os.chdir("no_perm")
     os.mkdir("a folder")
@@ -21,13 +24,23 @@ def test_no_permission_in_current_folder():
     os.chdir("..")
     os.rmdir("no_perm")
 
+
 def test_no_permission_in_sub_folder():
+    # create directories
     os.mkdir("perm")
     os.chdir("perm")
     os.mkdir("perm2")
     os.chdir("perm2")
     os.mkdir("a folder")
     os.chdir("..")
-    os.chmod("don't_search", mode=111)
-    assert searchLocal("a folder") is 0 # should return false, because no such file exists
-    shutil.rmtree("no_permission", True)
+    os.chmod("perm2", mode=222)
+    os.chdir("..") # We will be in perm folder, searching for "a folder" inside the non-readable perm2 folder
+    assert searchLocal("a folder") is 0 # should return false, because we can't search perm2
+    # cleanup
+    os.chdir("perm")
+    os.chdir("perm2")
+    os.rmdir("a folder")
+    os.chdir("..")
+    os.rmdir("perm2")
+    os.chdir("..")
+    os.rmdir("perm")
