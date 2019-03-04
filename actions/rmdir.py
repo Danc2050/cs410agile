7dd4003fc88d9a84e652f67c546a5a4fe11354f6
Danc2050 <danc2@pdx.edu>
2019-03-03 20:33:51 -0800

    Tests complete and re-worked code.

--- actions/rmdir.py
+++ actions/rmdir.py
@@ -1,16 +1,13 @@
 #Remove Directory
-import pysftp
 
 usage = "Error: no such file or directory contains content at "
 
 # Remove directory on server
 def rmdir(sftp, directory):
+    """Removes a directory either with the file path or the name of the directory
+    """
     try:
-        listCurrentDir = sftp.pwd                         # extract the current directory string from list
-        #fullpath = listCurrentDir + "/" + directory       # create a string of command rm -r /u/../directory
-        #sftp.rmdir(directory)
-        print('rm -rf ' + directory)
-        sftp.execute('rm -rf ' + directory)
+        sftp.rmdir(directory)
         return True
     except OSError as e:
         if e.strerror is None:
--- tests/unit/test_rmdir.py
+++ tests/unit/test_rmdir.py
@@ -1,27 +1,34 @@
-import pysftp
-import pytest
 from actions import rmdir
 
 # TEST 1: Directory exists
+
+
 def test_delete_empty_directory_succeeds(sftp):
 
     directory = "test_delete_empty_directory_succeeds"
     sftp.mkdir(directory, mode=777)                 # add a directory
-    rmdir.rmdir(sftp, directory)                    # remove directory
-    assert 2 == 2
+    assert rmdir.rmdir(sftp, directory) is True     # remove directory
+    # Cleanup
+    rmdir.rmdir(sftp, directory)
 
 # TEST 2: Directory does not exist
+
+
 def test_delete_non_exisitance_directory_fail(sftp):
     directory = "test_no_directory_fail"
+    assert rmdir.rmdir(sftp, directory) is False
+    # Cleanup
     rmdir.rmdir(sftp, directory)
-    assert 2 == 2
 
 # TEST 3: Directory has content
+
+
 def test_del_directory_with_content(sftp):
-    directory2 = "test_content_directory_fail/test"
     directory1 = "test_content_directory_fail"
-    sftp.mkdir(directory2, mode=777)
-    rmdir.rmdir(sftp,directory1)                          # test delete directory
-    rmdir.rmdir(sftp, directory2)                         # delete directory2
-    rmdir.rmdir(sftp, directory1)                         # delete directory1
-    assert 2 == 2
+    directory2 = "test_content_directory_fail/test"
+    sftp.mkdir(directory1, mode=777) # Create a directory
+    sftp.mkdir(directory2, mode=777) # Create a sub-directory
+    assert rmdir.rmdir(sftp, directory1) is False   # test delete directory which has content in it ("test")
+    # Cleanup
+    rmdir.rmdir(sftp, directory2)
+    rmdir.rmdir(sftp, directory1)
