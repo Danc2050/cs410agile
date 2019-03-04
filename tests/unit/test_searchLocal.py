0e0f4d1ad82bb22a836fb0278f3e885cf04076fd
Danc2050 <danc2@pdx.edu>
2019-03-04 11:22:30 -0800

    test_no_permission_in_current_folder works.

--- tests/unit/test_searchLocal.py
+++ tests/unit/test_searchLocal.py
@@ -1,4 +1,6 @@
 from actions.searchLocal import searchLocal
+import os
+import shutil
 
 def test_search_existing_file():
     flag = searchLocal("test")
@@ -7,3 +9,23 @@ def test_search_existing_file():
 def test_search_non_existant_file():
     flag = searchLocal("alkdsjhfasdklfhadshfaldksf")
     assert flag < 1
+
+def test_no_permission_in_current_folder():
+    os.mkdir("no_perm")
+    os.chdir("no_perm")
+    os.mkdir("a folder")
+    os.chmod(".", mode=111)
+    assert searchLocal("a folder") is 0 # the file will return
+    os.chdir("..")
+    shutil.rmtree("no_perm", True)
+
+def test_no_permission_in_sub_folder():
+    os.mkdir("permission")
+    os.chdir("permission")
+    os.mkdir("don't_search")
+    os.chdir("dont_search")
+    os.mkdir("a folder")
+    os.chdir("..")
+    os.chmod("don't_search", mode=111)
+    assert searchLocal("a folder") is 0 # should return false, because no such file exists
+    shutil.rmtree("no_permission", True)
