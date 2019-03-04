6e7f49b08a3e29153227fdcaf18f8af46c9b6be8
shwetapaliwal28 <spaliwal@pdx.edu>
2019-03-04 13:22:21 -0800

    added new test case to handle absolute path

--- tests/unit/test_put_file_onto_remote_server.py
+++ tests/unit/test_put_file_onto_remote_server.py
@@ -1,6 +1,7 @@
 import pysftp
 import pytest
 import paramiko
+import os
 from actions import put_file_onto_remote_server as put
 
 
@@ -10,6 +11,24 @@ def test_no_file(sftp, capsys):
     """
     assert put.put(sftp, "totes.not.fake.no.really") is False
     assert put.ERROR_MESSAGE in capsys.readouterr().out
+
+def test_put_file_with_absolute_path(sftp, capsys):
+    """Test what happens when you ask to transfer a
+    file which doesn't exist.
+    """
+    test_dir="folder1/folder2"
+    file_test = "file_test.txt"
+    os.makedirs(test_dir)
+    open(test_dir + '/' + file_test, 'a').close()
+
+    assert put.put(sftp, test_dir + '/' + file_test) is True
+    assert sftp.isfile(file_test) is True
+
+    sftp.remove(file_test) 
+
+    os.remove(test_dir + '/' + file_test)
+    os.removedirs(test_dir)
+
 
 
 def test_best_case(sftp):
