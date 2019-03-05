b4b1197e28b4cf25adb1b0522e8caf07b6757ef9
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-03-04 17:43:59 -0800

    Fixed mget. All tests pass.

--- actions/get_multiple_files_from_remote_server.py
+++ actions/get_multiple_files_from_remote_server.py
@@ -10,10 +10,17 @@ def get_multiple(sftp: pysftp.Connection, filenames: []):
     success = False
 
     for filename in filenames:
-        ret = get.get(sftp, filename)
-        if not ret:
-            print("filename in question:" + filename)
-        else:
-            success = True
+        # Print a header, without the newline
+        print("Getting " + filename + ": ", end="")
+        try:
+            if get.get(sftp, filename):
+                print("OK")
+                success = True
+            # Else, get() printed an error message.
+        except OSError as e:
+            if e.strerror is None:
+                print("Couldn't put file.")
+            else:
+                print(e.strerror)
 
     return success
