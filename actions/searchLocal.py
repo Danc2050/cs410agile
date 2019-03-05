2ec945742ba7725718ae676245fb4240408dcf26
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-03-04 18:20:03 -0800

    Change match criteria and print relative paths instead of absolute paths.

--- actions/searchLocal.py
+++ actions/searchLocal.py
@@ -1,19 +1,22 @@
 # Search local Directory and subdirectories
 import os
 
+
 def searchLocal(name):
     rootdir = os.getcwd()
     count = 0
     for subdir, dirs, files in os.walk(rootdir):
         filepath = subdir + os.sep
         for dir in dirs:
-            if dir.endswith(name):
-                print(filepath + dir)
+            if name in dir:
+                # Directory match! Print the relative path.
+                print(os.path.relpath(filepath + dir))
                 count = count + 1
         for file in files:
             filepath = subdir + os.sep + file
-            if file.startswith(name):
-                print(filepath)
+            if name in file:
+                # File match! Print the relative path.
+                print(os.path.relpath(filepath))
                 count = count + 1
     if count < 1:
         print("File not found")
